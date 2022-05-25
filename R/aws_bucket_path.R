e75dd0ed22a1d7c4b1c93f51f1ab830b52f3d710
harin-git <hlee@cbs.mpg.de>
2022-05-25 18:30:14 +0900

    new path to be added

---
+++ R/aws_bucket_path.R
@@ -0,0 +1,4 @@
+# Initialize the AWS buckets to use
+#' This directory should be changed to where it should be hosted
+audio <- "https://hleetestbuck.s3.eu-central-1.amazonaws.com/TPT_resources/audio/"
+image <- "https://hleetestbuck.s3.eu-central-1.amazonaws.com/TPT_resources/image/"
--- R/instructions.R
+++ R/instructions.R
@@ -109,7 +109,3 @@ instructions <- function(with_volume_headphone_check = FALSE,
   }
 }
 
-# Initialize the AWS buckets to use
-#' This directory should be changed to where it should be hosted
-audio <- "https://hleetestbuck.s3.eu-central-1.amazonaws.com/TPT_resources/audio/"
-image <- "https://hleetestbuck.s3.eu-central-1.amazonaws.com/TPT_resources/image/"
